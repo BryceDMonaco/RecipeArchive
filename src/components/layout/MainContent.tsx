d6e947e82790af6105ff0bf4e1d30a300dd81292
Bryce Monaco <brycedmonaco@gmail.com>
2026-03-18 21:42:49 -0700

    Fixed sidebar button renders on top of recipe titles

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -5,7 +5,7 @@ interface MainContentProps {
 export function MainContent({ children }: MainContentProps) {
   return (
     <main className="flex h-screen flex-1 flex-col overflow-y-auto bg-background">
-      <div className="mx-auto w-full max-w-3xl p-6 md:p-8">{children}</div>
+      <div className="mx-auto w-full max-w-3xl px-6 pb-6 pt-16 md:p-8">{children}</div>
     </main>
   );
 }
